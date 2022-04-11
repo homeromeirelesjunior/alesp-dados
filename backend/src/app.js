93fd7b03a182dd471f99aac8dc4851da89a29bf0
Homero Meireles <homero.meireles@novahaus.com.br>
2022-04-11 17:56:45 -0300

    testando-queries

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,13 +2,17 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import sqlite3 from 'sqlite3'
 
-const db = new sqlite3.Database('../dl-xml-db/db/db-deputados.db')
+const db = new sqlite3.Database('../db/db-deputados.db')
 const router = express.Router()
 const app = express()
 
 app.use(express.json())
 
-router.get('/deputados', (req, res) => {
+app.get('/', (req, res) => {
+    res.status(200).send('Tratamento de dados Alesp')
+})
+
+app.get('/deputados', (req, res) => {
     res.setHeader('Content-Type', 'application/json')
 
     try {
@@ -18,7 +22,43 @@ router.get('/deputados', (req, res) => {
            }
 
            res.end(JSON.stringify(rows))
-        })        
+        }) 
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+app.get('/deputados/:id', (req, res) => {
+    res.setHeader('Content-Type', 'application/json')
+    let id = req.params.id
+
+    try {
+        db.all(`SELECT * FROM deputados WHERE Id =  ?`, [id], (err, rows) => {
+           if (err) {
+               console.log(err)
+           }
+
+           res.end(JSON.stringify(rows))
+        }) 
+    } catch (error) {
+        console.log(error)
+    }    
+})
+
+app.get('/deputados/', (req, res) => {
+    res.setHeader('Content-Type', 'application/json')
+    let deputado = req.query.deputado
+
+    try {
+        db.all('SELECT * FROM deputados WHERE Deputado LIKE %?%', [deputado], (err, rows) => {
+            if (err) {
+                console.log(err)
+            }
+
+            res.send(deputado)
+            res.end(JSON.stringify(rows))
+
+        })
     } catch (error) {
         console.log(error)
     }
--- dl-dados/json-into-db.js
+++ dl-dados/json-into-db.js
@@ -2,7 +2,7 @@ const fs = require('fs')
 const sqlite3 = require('sqlite3').verbose()
 const db = new sqlite3.Database('../db/db-deputados.db')
 
-const despesasJSON = fs.readFileSync('./despesas_gabinetes.json')
+const despesasJSON = fs.readFileSync('./despesas_gabinetes-redux.json')
 
 let count = 0
 // console.log(typeof despesasJSON)
@@ -11,25 +11,24 @@ let count = 0
 
 const despesasArray = JSON.parse(despesasJSON)
 
-// despesasArray.despesas.despesa.forEach(item => {
-//    console.log(count++)             
-//   db.run(`INSERT INTO deputados VALUES (null, ?, ?, ?, ?, ?, ?, ?, ?)`, 
-//   [item.Ano, item.Matricula, item.Mes, item.Valor, item.CNPJ, item.Deputado, item.Tipo, item.Fornecedor], (err) => {
-//       count++
-//       if (err) {
-//           console.log(`Erro: ${ err }`)
-//           // res.end(JSON.stringify({ success: false }))
-//       } else {
-//           console.log('Ok')
-//           console.log(count)
-//           // res.end(JSON.stringify({ success: true }))
-//       }
-//   })
-// })
+despesasArray.despesas.despesa.forEach(item => {            
+  db.run(`INSERT INTO deputados VALUES (null, ?, ?, ?, ?, ?, ?, ?, ?)`, 
+  [item.Ano, item.Matricula, item.Mes, item.Valor, item.CNPJ, item.Deputado, item.Tipo, item.Fornecedor], (err) => {
+      count++
+      if (err) {
+          console.log(`Erro: ${ err }`)
+          // res.end(JSON.stringify({ success: false }))
+      } else {
+          console.log('Ok')
+          console.log(count)
+          // res.end(JSON.stringify({ success: true }))
+      }
+  })
+})
 
-arrayCompleto = despesasArray.despesas.despesa
+// arrayCompleto = despesasArray.despesas.despesa
 
-arrayLength = arrayCompleto.length
+// arrayLength = arrayCompleto.length
 
 // for (let i = 1; i < arrayLength; i+= 1000) {
 
@@ -38,18 +37,18 @@ arrayLength = arrayCompleto.length
 //    }
 // }
 
-let values = {
-   Ano: '',
-   Matricula: '',
-   Mes: '',
-   Valor: 0,
-   CNPJ: '',
-   Deputado: '',
-   Tipo: '',
-   Fornecedor: ''
-}
+// let values = {
+//    Ano: '',
+//    Matricula: '',
+//    Mes: '',
+//    Valor: 0,
+//    CNPJ: '',
+//    Deputado: '',
+//    Tipo: '',
+//    Fornecedor: ''
+// }
 
-let fullValues = []
+// let fullValues = []
 
 // arrayCompleto.forEach(item => {   
 //    values.Ano = item.Ano
@@ -65,30 +64,38 @@ let fullValues = []
 // });
 
 // console.log(fullValues)
-const sql = 'INSERT INTO deputados VALUES (null, ?, ?, ?, ?, ?, ?, ?, ?)'
+// const sql = 'INSERT INTO deputados VALUES (null, ?, ?, ?, ?, ?, ?, ?, ?)'
 
-arrayCompleto.forEach(item => {   
-   values.Ano = item.Ano
-   values.Matricula = item.Matricula
-   values.Mes = item.Mes
-   values.Valor = item.Valor
-   values.CNPJ = item.CNPJ
-   values.Deputado = item.Deputado
-   values.Tipo = item.Tipo
-   values.Fornecedor = item.Fornecedor
+// arrayCompleto.forEach(item => {   
+//    values.Ano = item.Ano
+//    values.Matricula = item.Matricula
+//    values.Mes = item.Mes
+//    values.Valor = item.Valor
+//    values.CNPJ = item.CNPJ
+//    values.Deputado = item.Deputado
+//    values.Tipo = item.Tipo
+//    values.Fornecedor = item.Fornecedor
 
-   fullValues.push(values)
-});
+//    fullValues.push(values)
+// });
 
 // console.log(fullValues[0].Ano)
 
-let valuesSQL = []
+// let valuesSQL = []
 
-for (let i = 0; i < 100; i++) {
-   valuesSQL.push(fullValues[i].Ano, fullValues[i].Matricula, fullValues[i].Mes, fullValues[i].Valor, fullValues[i].CNPJ, fullValues[i].Deputado, fullValues[i].Tipo, fullValues[i].Fornecedor)
-}
+// for (let i = 0; i < 10000; i++) {
+//    valuesSQL.push(fullValues[i].Ano, fullValues[i].Matricula, fullValues[i].Mes, fullValues[i].Valor, fullValues[i].CNPJ, fullValues[i].Deputado, fullValues[i].Tipo, fullValues[i].Fornecedor)
+// }
 
-console.log(valuesSQL)
+// let newValue = valuesSQL.join(', ')
+
+// db.run(sql, [newValue], (err) => {
+//    if (err) {
+//        console.log(`Erro: ${ err }`)
+//    } else {
+//        console.log('Ok')
+//    }
+// })
 
 
 // db.run(sql, [fullValues[0].Ano, fullValues[0].Matricula, fullValues[0].Mes, fullValues[0].Valor, fullValues[0].CNPJ, fullValues[0].Deputado, fullValues[0].Tipo, fullValues[0].Fornecedor], (err) => {
